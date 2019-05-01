84718285de5be20ae72a4bf44fdfd1cf151bb3b0
israxx97 <isragarcia97@gmail.com>
2019-05-01 15:03:36 +0200

    v0.1 - 01/05/2019, 15:02

---
+++ model/DBPDO.php
@@ -0,0 +1,25 @@
+<?php
+
+require_once 'config/configDB_localhost.php';
+require_once 'model/Errores.php';
+
+class DBPDO {
+
+    public static function ejecutaConsulta($sentenciaSQL, $parametros) {
+        try {
+            $miDB = new PDO(HOST_DB_localhost, USER_DB_localhost, PASS_DB_localhost);
+            $miDB->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $statement = $miDB->prepare($sentenciaSQL);
+            $statement->execute($parametros);
+        } catch (PDOException $pdoe) {
+            $statement = null;
+            Errores::anadirError($pdoe->getCode(), $pdoe->getMessage(), $pdoe->getFile(), $pdoe->getLine());
+            $_SESSION['pagina'] = 'error';
+            header('Location: index.php');
+        } finally {
+            unset($miDB);
+        }
+        return $statement;
+    }
+
+}
